31f8e744411a4cc19d9fa81fb28b64b53f4a23cd
Alastair Kitchen <alastair.kitchen@serialaffinity.co.uk>
2024-12-31 10:50:58 +0000

    add text data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import { getContent } from "@/app/lib";
 import { Button } from "@my-components/button";
+
+type Cat = {
+  name: string;
+  description: string;
+};
 
 export default async function Home() {
   const { data } = getContent("home");
@@ -10,6 +15,10 @@ export default async function Home() {
   return (
     <>
       <Heading>Hello world</Heading>
+
+      {data.cats.map((cat: Cat, i: number) => (
+        <Text key={i}>{cat.name}</Text>
+      ))}
       <Button>Click me</Button>
     </>
   );
